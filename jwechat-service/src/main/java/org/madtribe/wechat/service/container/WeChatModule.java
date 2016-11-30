f3831720871ac38e511fc30b10ca0685efbc7dee
MadTribe <MadTribe@users.noreply.github.com>
2016-12-01 00:21:48 +0800

    Set up guice module.

--- jwechat-service/src/main/java/org/madtribe/wechat/service/container/WeChatModule.java
+++ jwechat-service/src/main/java/org/madtribe/wechat/service/container/WeChatModule.java
@@ -2,7 +2,10 @@ package org.madtribe.wechat.service.container;
 
 
 import com.google.inject.AbstractModule;
+
 import org.madtribe.wechat.core.configuration.WeChatConfiguration;
+import org.madtribe.wechat.core.messageparsers.*;
+import org.madtribe.wechat.core.messageparsers.wc3.WeChatInboundRequestW3CParser;
 import org.madtribe.wechat.service.WechatExampleConfig;
 
 /**
@@ -20,6 +23,8 @@ public class WeChatModule extends AbstractModule {
 
     @Override
     protected void configure() {
+    	bind(InboundPayloadParserRegistry.class).to(DefaultInboundPayloadParserRegistry.class);
+    	bind(WeChatInboundMessageParser.class).to(WeChatInboundRequestW3CParser.class);
         bind(WeChatConfiguration.class).toInstance(configuration.getWeChatConfiguration());
     }
 
