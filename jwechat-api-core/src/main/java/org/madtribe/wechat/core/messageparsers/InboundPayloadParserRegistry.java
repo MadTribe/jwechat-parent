b2f2769ecc5a00105288b48b3a5e7cac862bb8c4
MadTribe <MadTribe@users.noreply.github.com>
2016-12-01 18:18:01 +0800

    added payload parser

---
+++ jwechat-api-core/src/main/java/org/madtribe/wechat/core/messageparsers/InboundPayloadParserRegistry.java
@@ -0,0 +1,11 @@
+package org.madtribe.wechat.core.messageparsers;
+
+import java.util.Optional;
+
+import org.madtribe.wechat.core.messageparsers.wc3.InboundPayloadParser;
+
+public interface InboundPayloadParserRegistry {
+
+	public Optional<InboundPayloadParser> lookup(String textContent);
+
+}
