3e3487cb95fcfd303c434852486456b0aa45e62f
(none) <(none)>
2016-12-13 18:38:48 +0800

    Refactored to cleaner form

--- jwechat-service/src/main/java/org/madtribe/wechat/service/WechatExampleApplication.java
+++ jwechat-service/src/main/java/org/madtribe/wechat/service/WechatExampleApplication.java
@@ -4,7 +4,6 @@ import de.thomaskrille.dropwizard.environment_configuration.EnvironmentConfigura
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-
 import javax.ws.rs.core.Response;
 
 import org.madtribe.wechat.core.container.JWeChatContanerMain;
@@ -13,13 +12,17 @@ import org.madtribe.wechat.core.messages.TextMessage;
 import org.madtribe.wechat.core.messages.inbound.request.InboundRequest;
 import org.madtribe.wechat.core.resources.WeChatEntryPoint;
 import org.madtribe.wechat.service.resources.RootResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 
 /**
  * Created by paul.smout on 04/04/2016.
  */
 public class WechatExampleApplication extends Application<WechatExampleConfig> {
-    private JWeChatContanerMain guiceMain;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WechatExampleApplication.class);
 
     public static void main(String[] args) throws Exception {
 
@@ -38,28 +41,25 @@ public class WechatExampleApplication extends Application<WechatExampleConfig> {
 
     @Override
     public void run(WechatExampleConfig configuration, Environment environment) throws Exception {
-        try {
+
+        WeChatEntryPoint entryPoint = configureEntryPoint(configuration, environment);
+
+        // This is where you can register your WeChat message hanlders.
+        // These can obviously be put in other classes as required.
+        entryPoint.handle("text", (InboundRequest message) -> {
+        	   return Response.ok("Client Code has received a message: " + message).build();
+        });
 
 
-            environment.jersey().register(new RootResource(configuration));
-
-
-            guiceMain = new JWeChatContanerMain( configuration.getWeChatConfiguration());
-            WeChatEntryPoint entryPoint = guiceMain.get(WeChatEntryPoint.class);
-            environment.jersey().register(guiceMain.get(WeChatInboundRequestReader.class));
-            environment.jersey().register(entryPoint);
-
-            entryPoint.handle("text", (InboundRequest message) -> {
-            	System.err.println("Client Code has received a message: " + message);
-
-            	return Response.ok("Client Code has received a message: " + message).build();
-            });
-
-
-        } catch (Throwable t){
-            t.printStackTrace();
-        }
     }
 
+    private WeChatEntryPoint configureEntryPoint(WechatExampleConfig configuration, Environment environment) throws ClassNotFoundException {
+        JWeChatContanerMain guiceMain = new JWeChatContanerMain( configuration.getWeChatConfiguration());
+        environment.jersey().register(new RootResource(configuration));
+        WeChatEntryPoint entryPoint = guiceMain.get(WeChatEntryPoint.class);
+        environment.jersey().register(guiceMain.get(WeChatInboundRequestReader.class));
+        environment.jersey().register(entryPoint);
+        return entryPoint;
+    }
 
 }
