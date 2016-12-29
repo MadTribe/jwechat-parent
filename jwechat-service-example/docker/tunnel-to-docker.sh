d9b4c557a401a6b4dd12e8a8993add9d6574e835
= <=>
2016-12-29 22:44:49 +0800

    improvements to tunnel-to-docker.sh so a fixed test domain can be configured.

--- jwechat-service-example/docker/tunnel-to-docker.sh
+++ jwechat-service-example/docker/tunnel-to-docker.sh
@@ -1,3 +1,9 @@
+if [ -z ${WECHAT_REQUESTED_SDOMAIN+x} ]; then
+    echo "WECHAT_REQUESTED_SDOMAIN not set so will use random one";
+    WECHAT_REQUESTED_SDOMAIN="";
+else 
+    WECHAT_REQUESTED_SDOMAIN="-s $WECHAT_REQUESTED_SDOMAIN";
+fi
 
 #DOCKER_IP=$(echo $DOCKER_HOST | cut -d":" -f2 | cut -d"/" -f3 )
 DOCKER_IP=127.0.0.1
@@ -8,5 +14,5 @@ echo "Configure Wechat callback to point to <tunnel url below>/wxcallback"
 
 alias node=nodejs
 
-lt -l $DOCKER_IP -p 8080
+lt -s $WECHAT_REQUESTED_SDOMAIN -l $DOCKER_IP -p 8080
 
